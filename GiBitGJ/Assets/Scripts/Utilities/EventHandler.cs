c07418bc9d5cb470b5241e2917be772af43f3cd6
Forgoi <146811419+Forgoi@users.noreply.github.com>
2024-11-27 19:06:46 +0800

    背包物品保存完成

--- GiBitGJ/Assets/Scripts/Utilities/EventHandler.cs
+++ GiBitGJ/Assets/Scripts/Utilities/EventHandler.cs
@@ -5,10 +5,24 @@ using System;
 
 public class EventHandler
 {
-    public static event Action<ItemDetails,int> UpdateUIEvent;
+    public static event Action<ItemDetails, int> UpdateUIEvent;
 
     public static void CallUpdateUIEvent(ItemDetails itemDetails, int index)
     {
-        UpdateUIEvent?.Invoke(itemDetails, index);  
+        UpdateUIEvent?.Invoke(itemDetails, index);
+    }
+
+    public static event Action BeforeSceneUnloadEvent;
+
+    public static void CallBeforeSceneUnloadEvent()
+    {
+        BeforeSceneUnloadEvent?.Invoke();
+    }
+
+    public static event Action AfterSceneLoadedEvent;
+
+    public static void CallAfterSceneLoadedEvent()
+    {
+        AfterSceneLoadedEvent?.Invoke();
     }
 }
