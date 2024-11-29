0439ab0f6c13bddb633147d7e3a21af11ee7595d
Forgoi <146811419+Forgoi@users.noreply.github.com>
2024-11-30 02:23:27 +0800

    特殊房间处理已完成

--- GiBitGJ/Assets/Scripts/WindowExitScript.cs
+++ GiBitGJ/Assets/Scripts/WindowExitScript.cs
@@ -21,7 +21,7 @@ public class WindowExitScript : MonoBehaviour
     {
         int levelPosition = LevelToLevelData.levelToNum[currentLevel];
 
-        /*
+
         //对于特殊房间进行特判
         if(windowOnOrUnder == 0 && levelPosition == 8 && windowOrder == 2 && LevelToLevelData.IsWindowOpened("0level", 1, windowOrder) && LevelToLevelData.boolArray[0])
         {
@@ -43,8 +43,18 @@ public class WindowExitScript : MonoBehaviour
 
             return;
         }
-        */
+        else if (windowOnOrUnder == 1 && levelPosition == 0 && windowOrder == 2 && LevelToLevelData.stringArray[8] == "2level")
+        {
+            canPass = true;
+            //将门的颜色设置为黄色
+            gameObject.GetComponent<SpriteRenderer>().color = new Color(1, 1, 0, 1);
 
+            nextLevel = "2level";
+
+            return;
+        }
+
+        //////////////////////////////////////////////////////////分割线//////////////////////////////////////////////////////////
 
         //n 是行，m 是列
         level_m = levelPosition % 3;
