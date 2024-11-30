2f029e9f3facd77326e791d00ebf1cbec6b2d92d
Forgoi <146811419+Forgoi@users.noreply.github.com>
2024-12-01 04:51:48 +0800

    111

---
+++ GiBitGJ/Assets/Scripts/StartScript.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartScript : MonoBehaviour
+{
+        void Start()
+        {
+            // º”‘ÿH1≥°æ∞
+            SceneManager.LoadScene("StartScene", LoadSceneMode.Additive);
+        }
+
+
+}
